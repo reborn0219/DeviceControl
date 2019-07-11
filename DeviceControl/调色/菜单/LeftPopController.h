bd2178de99cf63cc100ca64dabf175c2bff4832a
liushuai <258708917@qq.com>
2019-07-11 11:19:53 +0800

    no message

--- DeviceControl/调色/菜单/LeftPopController.h
+++ DeviceControl/调色/菜单/LeftPopController.h
@@ -11,6 +11,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface LeftPopController : UIViewController
+@property (nonatomic,strong) NSArray *blueArr;
 -(void)showInVC:(UIViewController *)VC;
 
 @end
--- DeviceControl/调色/菜单/LeftPopController.m
+++ DeviceControl/调色/菜单/LeftPopController.m
@@ -8,7 +8,8 @@
 
 #import "LeftPopController.h"
 
-@interface LeftPopController ()
+@interface LeftPopController ()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *leftTable;
 
 @end
 
@@ -16,8 +17,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    _leftTable.delegate = self;
+    _leftTable.dataSource = self;
+    
 }
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self.leftTable reloadData];
+}
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+}
+
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     [self dismissViewControllerAnimated:NO completion:nil];
 }
@@ -37,5 +48,40 @@
     }
     [VC presentViewController:self animated:YES completion:nil];
 }
-
+#pragma mark - TableViewDelegate
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    
+    UIView * sectionView = [[UIView alloc]init];
+    [sectionView setBackgroundColor:[UIColor clearColor]];
+    UIImageView * ligimgV = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 40, 40)];
+    [ligimgV setImage:[UIImage imageNamed:@""]];
+    [sectionView addSubview:ligimgV];
+    UILabel * liglabel = [[UILabel alloc]initWithFrame:CGRectMake(40, 0, 80, 40)];
+    liglabel.text = @"我的设备";
+    [sectionView addSubview:liglabel];
+    
+    return sectionView;
+}
+-(CGFloat)tableView:(UITableView *)tableView estimatedHeightForHeaderInSection:(NSInteger)section{
+    return 40;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return _blueArr.count;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"];
+    cell.backgroundColor  =[UIColor clearColor];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.textLabel.text = [_blueArr objectAtIndex:indexPath.row];
+    return cell;
+    
+}
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+}
 @end
